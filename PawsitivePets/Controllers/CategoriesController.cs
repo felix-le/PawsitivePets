c235aa5816727fdfca78e52cb1c58a9577230216
ifotn <rich@infrontofthenet.com>
2021-09-16 12:48:55 -0400

    l2/2 - browse categories, category model

--- PawsitivePets/Controllers/CategoriesController.cs
+++ PawsitivePets/Controllers/CategoriesController.cs
@@ -12,5 +12,21 @@ namespace PawsitivePets.Controllers
         {
             return View();
         }
+
+        // add a new method that's called when user clicks a category from the list displayed
+        // method accepts a string value in the url called "category"
+        public IActionResult Browse(string category)
+        {
+            // if category is empty / missing, take user back to category list
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Store the input parameter in the ViewBag to display in the view
+            ViewBag.category = category;
+
+            return View();
+        }
     }
 }
